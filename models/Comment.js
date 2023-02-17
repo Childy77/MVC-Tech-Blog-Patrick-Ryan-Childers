92c8f9174ca8b21a2f46c6ac869344abc3c52d55
Ryan <ryanbairdchilders@gmail.com>
2023-02-17 12:22:25 -0800

    add data for comment section

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,13 +11,10 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+    
     content: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     date_posted: {
       type: DataTypes.DATE,
@@ -38,8 +35,8 @@ Post.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post',
+    modelName: 'comment',
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
